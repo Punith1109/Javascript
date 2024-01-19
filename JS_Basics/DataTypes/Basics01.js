4f2100872e2dbac03a4287cc6e791629980aea06
Punith1109 <gr3062@srmist.edu.in>
2024-01-19 10:44:32 +0530

    1st Javascript

---
+++ JS_Basics/DataTypes/Basics01.js
@@ -0,0 +1,28 @@
+//Variables,Declarations
+// let a;
+// var b=" ";
+// const d=0;
+
+// Data_Types
+let floatNum = 3.14; 
+let anotherFloat = 2.0; 
+
+let integerNum = 42; // Integer
+let text = "Locobuzz";
+
+let isTrue = true;
+
+
+let undefinedVar;
+
+let nullVar = null;
+
+let symbolVar = Symbol('uniqueSymbol');
+
+console.log(floatNum);
+console.log(integerNum);
+console.log(text);
+console.log(isTrue);
+console.log(undefinedVar);
+console.log(nullVar);
+console.log(symbolVar);
---
+++ JS_Basics/DataTypes/Basics02.js
@@ -0,0 +1,26 @@
+
+// function sum(n1,n2){
+//     return n1=n2;
+// }
+
+// console.log(sum(1,2))
+
+// Arrow Function
+// let add = (a, b) => a + b;
+
+// let square = x => x * x;
+// const sum = add(5, 3);
+// let squaredValue = square(4);
+
+// console.log( sum);
+// console.log(squaredValue);
+
+
+setTimeout(function(){
+    console.log("Hii")
+},1)
+let k=0;
+for(let i=0;i<1000000000;i++){
+     k=k+1
+}
+console.log(k)
