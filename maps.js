72b3a0525b5a6b2870d782f6471cf56fa55bfdce
Punith1109 <gr3062@srmist.edu.in>
2024-02-03 15:29:02 +0530

    Map_Data Structure

--- maps.js
+++ maps.js
@@ -1,30 +1,29 @@
-const numbers = [1, 2, 3, 4, 5];
-const doubled = numbers.map(num => num * 2);
-console.log(doubled); 
-
-
-/////
-const words = ['hello', 'world'];
-
-const w=words.map((word)=>{
-    word=word.toLocaleLowerCase();
+let myset=new Set();
+myset.add(1);
+myset.add("punith")
+myset.add({
+    name:"Punith",
+    age:21
 })
-console.log(w)
-////
-const users=[
-    {
-        name:'Punith',
-        age:21
-    },{
-        name:'HAri',
-        age20
-    },
-    {
-        name:"Sai",
-        age:22,
-        Country:"India"
-    }
-]
-const ppl=users.map((user)=>user.name)
 
-console.log(ppl)
+console.log(myset.size); 
+
+console.log(myset.has("Punith"))
+
+myset.delete(1)
+
+
+console.log(myset.size); 
+/////////////////////
+const set1 = new Set([1, 2, 3, 4]);
+const set2 = new Set([3, 4, 5, 6]);
+
+const unionSet = new Set([...set1, ...set2]);
+console.log([unionSet]); 
+
+const intersectionSet = new Set([...set1].filter(value => set2.has(value)));
+console.log([...intersectionSet]); 
+
+
+const differenceSet = new Set([...set1].filter(value => !set2.has(value)));
+console.log([...differenceSet]); 
